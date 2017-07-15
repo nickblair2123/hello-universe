9f10c38ec306d072fbd94130468e08d83f0039c2
Nicholas Blair <Nicholas.H.Blair@Intel.com>
2017-07-14 19:10:30 -0700

    Create initial project: Create class HelloUnivers and assign planets to the list solarSystem

---
+++ src/HelloUniverse.java
@@ -0,0 +1,13 @@
+/**
+ * @author Nicholas Blair
+ * @version 7/14/2017.
+ */
+
+public class HelloUniverse {
+
+    public static void main(String[] arg) {
+        String[] solarSystem = {
+                "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
+        };
+    }
+}
